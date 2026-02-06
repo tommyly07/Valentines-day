f874b7d9fde9a830df76ed8ddda80bb4db781b89
tommyly07 <krylinnkhmer@gmail.com>
2026-02-06 21:12:52 +0100

    javascript fertig

---
+++ script.js
@@ -0,0 +1,29 @@
+//Mein erstes JavaScript
+
+const message =[
+    "Bist du dir sicher?",
+    "Willst du das wirklich tun?",
+    "Bist du dir wirklich sicher?",
+    "Bitte nicht...",
+    "Überleg es dir nochmal!",
+    "Wenn du nein sagst, dann bin ich traurig :(",
+    "Ich werde traurig",
+    "Ich werde sehr traurig :(",
+    "Ok ich frage nicht mehr",
+    "Spass, sag JA! ❤️",
+];
+
+let messageIndex = 0;
+
+function handleNoclick(){
+const nobutton = document.querySelector(".nobutton");  //Brücke zwischen HTML und JavaScript
+const yesbutton = document.querySelector(".yesbutton");
+nobutton.textContent = message[messageIndex];
+messageIndex = (messageIndex + 1) % message.length;
+const currentsize = parseFloat(getComputedStyle(yesbutton).fontSize); // Aktuelle Schriftgröße des Ja-Buttons, bsp.: 16px -> 16
+yesbutton.style.fontSize = `${currentsize * 1.5}px`;
+}
+
+function handleYesclick(){
+    window.location.href = "yes_page.html";
+}
